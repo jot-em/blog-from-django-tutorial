42a9c23f8837924376960cfcae55d91f47087188
Justyna Mazur <justyna.p.mazur@gmail.com>
2017-02-18 18:45:48 +0100

    Add css to blog

--- popkornowo/settings.py
+++ popkornowo/settings.py
@@ -101,4 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'pubic', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
