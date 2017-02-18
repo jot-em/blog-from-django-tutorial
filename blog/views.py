e88b3b3b5a303c129f9a1f8a602f828ef7755bf8
Justyna Mazur <justyna.p.mazur@gmail.com>
2017-02-18 20:38:19 +0100

    Fix indentation for python3

--- blog/views.py
+++ blog/views.py
@@ -13,15 +13,15 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
-	form = PostForm(request.POST)
-	if form.is_valid():
-	   post = form.save(commit=False)
-	   post.author = request.user
-	   post.published_date = timezone.now()
-	   post.save()
-	   return redirect('post_detail', pk=post.pk)
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
-	form = PostForm()
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form':form})  
   
 def post_edit(request, pk):
