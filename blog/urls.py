41eaee859d74e99a722c15629ddf45ee5aae30c5
Justyna Mazur <justyna.p.mazur@gmail.com>
2017-04-21 23:03:43 +0200

    login with default form

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -7,7 +8,7 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^login/$', views.login, name='login'),
-    # url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    # url(r'^login/$', views.login, name='login'),
+    url(r'^login/$', login, kwargs={'template_name': 'blog/login.html'}, name='login'),
     url(r'^post/category/$', views.category, name='category'),
 ]
