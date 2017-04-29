2e0f6c82b280af521a76c49dd1d41364ad2c066e
Justyna Mazur <justyna.p.mazur@gmail.com>
2017-04-29 21:46:42 +0200

    Fix validation of registration and some translations

--- blog/forms.py
+++ blog/forms.py
@@ -14,19 +14,20 @@ class RegistrationForm(forms.Form):
  
     username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=3)), label=_("Nazwa użytkownika"), 
     	error_messages={ 'invalid': _("Nazwa zawierać może wyłącznie litery, cyfry i znak podkreślenia.") })
-    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Adres e-mail"))
+    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Adres e-mail"), error_messages={ 'invalid': _("Niepoprawny adres e-mail.") })
     password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Hasło"))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Powtórz hasło"))
- 
+
     def clean_username(self):
         try:
             user = User.objects.get(username__iexact=self.cleaned_data['username'])
         except User.DoesNotExist:
             return self.cleaned_data['username']
-        raise forms.ValidationError(_("Hasła nie są identyczne."))
+        raise forms.ValidationError(_("Użytkownik o podanej nazwie już istnieje. Podaj inną nazwę."))
  
     def clean(self):
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError(_("Użytkownik o podanej nazwie już istnieje. Podaj inną nazwę."))
+                raise forms.ValidationError(_("Hasła nie są identyczne."))
         return self.cleaned_data
+
